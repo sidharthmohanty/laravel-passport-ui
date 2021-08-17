917f8c658994908b759c1a26fc75b7eb30c097d1
sidharthmohanty <sidharthmohanty21@gmail.com>
2021-08-17 22:24:30 +0530

    fixed login controller and passport integration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +27,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        Passport::routes();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
-
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api', 'namespace' => 'Api'], function(){
+    
+    Route::get('/user', 'LoginController@alluser');
+    
 });
+ 
+   
+Route::post('/login', [LoginController::class, "login"])->name('api.login');
+Route::post('/register', [LoginController::class, "register"])->name('api.register');
